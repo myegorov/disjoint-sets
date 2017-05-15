b9959b4da45126458147fa51c1331e5e05ecbed8
Maksim Yegorov <findmaksim@gmail.com>
2017-05-15 15:16:10 -0400

    mutation hidden within IO

--- src/PArray.hs
+++ src/PArray.hs
@@ -5,18 +5,36 @@
 Module      : PArray
 Description : Persistent Mutable Array
 License     : GNU GPLv3
-Maintainer  : findmaksim@gmail.com
+Author      : findmaksim@gmail.com
 
-Implement mutable persistent array with O(1) access operations
-as described in:
+Implements mutable persistent array (so called "trailer array"):
+  + store full array for each leaf node;
+  + and a history of updates at each node
+=> O(1) access to the most recent version of an array
+=> if reverse update pointers on first access to a previous version,
+    then amortized O(1) access
+
+For details, see:
+S. Conchon and J.-C. Filliâtre. A persistent union-find data structure. In:
+Proceedings of the 2007 Workshop on ML, pages 37–46. ACM, 2007.
 https://www.lri.fr/~filliatr/ftp/publis/puf-wml07.pdf
 
 -}
 
-module PArray where
+module PArray 
+( PArr(..)
+, AData(..)
+, fromList
+, len
+, create
+, initialize
+, get
+, set ) where
 
 import System.IO.Unsafe
-import Data.Array.IO
+import Data.Array.IO (IOArray, getElems, getBounds,
+                        newArray, newListArray,
+                        readArray, writeArray)
 import Data.IORef
 
 -- persistent mutable array data type
@@ -31,39 +49,40 @@ instance Show a =>  Show (PArr a) where
 toList :: PArr a -> [a]
 toList ref = 
     let xs = unsafePerformIO $ do
-                reroot ref
-                r <- readIORef ref
-                case r of
-                    Arr arr -> Data.Array.IO.getElems arr
+                    reroot ref
+                    r <- readIORef ref
+                    case r of
+                        Arr arr -> getElems arr
     in xs
 
 -- get length of current array
-len :: PArr a -> Int
-len ref = unsafePerformIO $ do
-    reroot ref
-    r <- readIORef ref
+len :: IO (PArr a) -> IO Int
+len ref = do
+    ref1 <- ref
+    reroot ref1
+    r    <- readIORef ref1
     case r of
         Arr arr -> do
-            (i, j) <- Data.Array.IO.getBounds arr
+            (i, j) <- getBounds arr
             return (j - i + 1)
         Diff _ _ _  -> error "len encountered Diff, puzzled..."
 
 -- create new PArr of length n, intialize all elements to value v
-create :: Int -> a -> PArr a
-create n v0 = unsafePerformIO $ do
-    arr <- Data.Array.IO.newArray (0, n-1) v0
+create :: Int -> a -> IO (PArr a)
+create n v0 = do
+    arr <- newArray (0, n-1) v0
     ref <- newIORef (Arr arr)
     return ref
 
 -- create new PArr from list
-fromList :: [a] -> PArr a
-fromList xs = unsafePerformIO $ do
-    arr <- Data.Array.IO.newListArray (0, length xs - 1) xs
+fromList :: [a] -> IO (PArr a)
+fromList xs = do
+    arr <- newListArray (0, length xs - 1) xs
     ref <- newIORef (Arr arr)
     return ref
 
 -- create new PArr of length n from function
-initialize :: Int -> (Int -> a) -> PArr a
+initialize :: Int -> (Int -> a) -> IO (PArr a)
 initialize n f =  let xs = map f [0..n-1]
             in fromList xs
 
@@ -71,7 +90,7 @@ initialize n f =  let xs = map f [0..n-1]
 -- modify pointers so as to be able to access array immediately
 reroot :: PArr a -> IO ()
 reroot ref = do
-    r <- readIORef ref
+    r    <- readIORef ref
     case r of
         Arr _            -> return ()
         Diff ix val ref' -> do
@@ -79,8 +98,8 @@ reroot ref = do
             r' <- readIORef ref'
             case r' of
                 a@(Arr arr) -> do
-                    oldVal <- Data.Array.IO.readArray arr ix
-                    Data.Array.IO.writeArray arr ix val
+                    oldVal <- readArray arr ix
+                    writeArray arr ix val
                     writeIORef ref a
                     writeIORef ref' (Diff ix oldVal ref)
                     return ()
@@ -89,34 +108,50 @@ reroot ref = do
 
 
 -- access element of PArr
--- get :: PArr a -> Int -> IO a
-get :: PArr a -> Int -> a
-get ref i = unsafePerformIO $ do
-    r <- readIORef ref
+get :: IO (PArr a) -> Int -> IO a
+get ref i = do
+    ref1 <- ref
+    r    <- readIORef ref1
     case r of 
-        Arr arr         -> Data.Array.IO.readArray arr i
+        Arr arr         -> readArray arr i
         Diff _ _ _      -> do
-            reroot ref
-            r' <- readIORef ref
+            reroot ref1
+            r' <- readIORef ref1
             case r' of
-                Arr arr     -> Data.Array.IO.readArray arr i
+                Arr arr     -> readArray arr i
                 Diff _ _ _  -> error "get encountered Diff, puzzled..."
 
 
 -- update operation returns a new persistent array 
-set :: (Eq a) => PArr a -> Int -> a -> PArr a
-set ref ix val = unsafePerformIO $ do
-    reroot ref
-    r <- readIORef ref
+set :: (Eq a) => IO (PArr a) -> Int -> a -> IO (PArr a)
+set ref ix val = do
+    ref1 <- ref
+    reroot ref1
+    r <- readIORef ref1
     case r of
         a@(Arr arr)    -> do
-                            oldVal  <- Data.Array.IO.readArray arr ix
+                            oldVal  <- readArray arr ix
                             -- no need to create new indirection if ix is already mapped to val
                             if oldVal == val
-                            then return ref
+                            then return ref1
                             else do
-                                Data.Array.IO.writeArray arr ix val
+                                writeArray arr ix val
                                 ref'    <- newIORef a
-                                writeIORef ref (Diff ix oldVal ref')
+                                writeIORef ref1 (Diff ix oldVal ref')
                                 return ref'
         Diff _ _ _     -> error "set encountered Diff, puzzled..."
+
+
+-- -- usage examples
+-- ghci> a0 = create 7 0
+-- ghci> a1 = set a0 1 7
+-- ghci> a2 = set a1 2 8
+-- ghci> a3 = set a1 2 9
+-- ghci> a0
+-- [0,0,0,0,0,0,0]
+-- ghci> a1
+-- [0,7,0,0,0,0,0]
+-- ghci> a2
+-- [0,7,8,0,0,0,0]
+-- ghci> a3
+-- [0,7,9,0,0,0,0]
--- src/PUnionFind.hs
+++ src/PUnionFind.hs
@@ -5,22 +5,29 @@
 Module      : PUnionFind
 Description : Persistent Union-Find
 License     : GNU GPLv3
-Maintainer  : findmaksim@gmail.com
+Author      : findmaksim@gmail.com
 
-Implement Tarjan's union-find algorithm using persistent arrays,
-as described in:
+Implements Tarjan's union-find algorithm using persistent arrays.
+
+For details, see:
+S. Conchon and J.-C. Filliâtre. A persistent union-find data structure. In:
+Proceedings of the 2007 Workshop on ML, pages 37–46. ACM, 2007.
 https://www.lri.fr/~filliatr/ftp/publis/puf-wml07.pdf
 
 -}
 
-module PUnionFind where
+module PUnionFind
+( UF(..)
+, createUF
+, union
+, find ) where
 
 import PArray
 
 data UF a where 
     UF :: Num a => { 
         rank :: PArr a,
-        parent :: PArr a 
+        parent :: PArr a
     } -> UF a
 
 instance Show a => Show (UF a) where
@@ -28,47 +35,83 @@ instance Show a => Show (UF a) where
         "<UF rank=" ++ show r ++ " parent=" ++ show p ++ ">"
 
 -- create new union-find structure
-createUF :: Int -> UF Int
-createUF size = let
-                    r = create size 0
-                    p = initialize size id
-                in UF {rank=r, parent=p}
+createUF :: Int -> IO (UF Int)
+createUF size = do
+                    r <- create size 0
+                    p <- initialize size id
+                    return (UF {rank=r, parent=p})
 
 
 -- a helper function to perform path compression
 -- returns mutated parent array and representative
-_find :: PArr Int -> Int -> (PArr Int, Int)
-_find par ix = let ix' = get par ix
-               in
+_find :: IO (PArr Int) -> Int -> IO (PArr Int, Int)
+_find par ix = do 
+                    par' <- par
+                    ix' <- get par ix
                     if ix' == ix
-                    then (par, ix)
-                    else
-                        let (par1, root) = _find par ix'
-                            par2         = set par1 ix root
-                        in (par2, root)
+                    then return (par', ix)
+                    else do
+                        (par1, root) <- _find par ix'
+                        par2         <- set (return par1) ix root
+                        return (par2, root)
 
 -- find with path compression
-find :: UF Int -> Int -> (UF Int, Int)
-find uf elem = let (par, representative) = _find (parent uf) elem
-               in (uf { parent=par }, representative)
+find :: IO (UF Int) -> Int -> IO (UF Int, Int)
+find uf elem = do
+                    uf1                   <- uf
+                    (par, representative) <- _find (return (parent uf1)) elem
+                    return (uf1 { parent=par }, representative)
 
 
 -- union by rank
-union :: UF Int -> Int -> Int -> UF Int
-union uf x y = 
-    let
-        (uf1, reprX) = find uf x
-        (uf2, reprY) = find uf1 y
-    in 
+union :: IO (UF Int) -> Int -> Int -> IO (UF Int)
+union uf x y = do
+        (uf1, reprX) <- find uf x
+        (uf2, reprY) <- find (return uf1) y
         if reprX == reprY
-        then uf2
-        else let
-                rankX = get (rank uf2) reprX
-                rankY = get (rank uf2) reprY
-                in
+        then return uf2
+        else do
+                rankX <- get (return (rank uf2)) reprX
+                rankY <- get (return (rank uf2)) reprY
                 if rankX > rankY
-                then uf2 { parent=(set (parent uf2) reprY reprX) }
+                then do
+                    par1  <- set (return (parent uf2)) reprY reprX
+                    return (uf2 { parent=par1 })
                 else if rankY > rankX
-                then uf2 { parent=(set (parent uf2) reprX reprY) }
-                else uf2 { rank=(set (rank uf2) reprX (rankX+1)), 
-                            parent=(set (parent uf2) reprY reprX) }
+                then do
+                    par1  <- set (return (parent uf2)) reprX reprY
+                    return (uf2 { parent=par1 })
+                else do
+                    rank1 <- set (return (rank uf2)) reprX (rankX+1)
+                    par1  <- set (return (parent uf2)) reprY reprX
+                    return (uf2 { rank=rank1, parent=par1 })
+
+-- -- usage examples
+-- ghci> t = createUF 10
+-- ghci> t
+-- <UF rank=[0,0,0,0,0,0,0,0,0,0] parent=[0,1,2,3,4,5,6,7,8,9]>
+-- ghci> find t 0
+-- (<UF rank=[0,0,0,0,0,0,0,0,0,0] parent=[0,1,2,3,4,5,6,7,8,9]>,0)
+-- ghci> find t 1
+-- (<UF rank=[0,0,0,0,0,0,0,0,0,0] parent=[0,1,2,3,4,5,6,7,8,9]>,1)
+-- ghci> t1 = union t 0 1
+-- ghci> t1
+-- <UF rank=[1,0,0,0,0,0,0,0,0,0] parent=[0,0,2,3,4,5,6,7,8,9]>
+-- ghci> find t1 0
+-- (<UF rank=[1,0,0,0,0,0,0,0,0,0] parent=[0,0,2,3,4,5,6,7,8,9]>,0)
+-- ghci> find t1 1
+-- (<UF rank=[1,0,0,0,0,0,0,0,0,0] parent=[0,0,2,3,4,5,6,7,8,9]>,0)
+-- ghci> t2 = union t1 2 3
+-- ghci> t2
+-- <UF rank=[1,0,1,0,0,0,0,0,0,0] parent=[0,0,2,2,4,5,6,7,8,9]>
+-- ghci> t3 = union t2 0 3
+-- ghci> t3
+-- <UF rank=[2,0,1,0,0,0,0,0,0,0] parent=[0,0,0,2,4,5,6,7,8,9]>
+-- ghci> (t4, _) <- find t3 3
+-- ghci> t4
+-- <UF rank=[2,0,1,0,0,0,0,0,0,0] parent=[0,0,0,0,4,5,6,7,8,9]>
+-- ghci> t5 = union (return t4) 4 4
+-- ghci> t5
+-- <UF rank=[2,0,1,0,0,0,0,0,0,0] parent=[0,0,0,0,4,5,6,7,8,9]>
+-- ghci> find (return t4) 4
+-- (<UF rank=[2,0,1,0,0,0,0,0,0,0] parent=[0,0,0,0,4,5,6,7,8,9]>,4)
